2f24443544f80c5f19f02402d2c059eacfcdd7a5
osamaelkassaby <osamaelkassaby11@gmail.com>
2023-03-02 00:47:56 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from "react";
 function App() {
+  useEffect(()=>{
+    window.paypal.Buttons({
+      creteOrder:(data,actions)=>{
+        return actions.order.create({
+          purchase_units:[{
+            amount:{
+              value:600.00,
+            }
+          }]
+        });
+      },
+      onApprove: function (data, actions) {
+        console.log("done")
+        return actions.order.capture().then( details =>{
+          window.open("http://osamaelkassaby.com");
+          alert("complete by "+details.payer.name.given_name)
+          console.log(details)
+        })
+      },
+    }).render('#paypal-button-container');
+  },[])
+// START ///
+
+
+/*code here */
+
+
+//HERER///
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div id="paypal-button-container">
+          <input type='text' />
+
     </div>
+    
   );
 }
 
