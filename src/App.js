3c2d4035c82d0f61a8d11f5524f40bb631ec35c9
ahmed borham <ahmedcosta317@gmail.com>
2023-03-01 22:59:37 +0000

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
 import React, { useEffect } from "react";
 function App() {
-  useEffect(()=>{
-    window.paypal.Buttons({
-      creteOrder:(data,actions)=>{
-        return actions.order.create({
-          purchase_units:[{
-            amount:{
-              value:600.00,
-            }
-          }]
-        });
-      },
-      onApprove: function (data, actions) {
-        console.log("done")
-        return actions.order.capture().then( details =>{
-          window.open("http://osamaelkassaby.com");
-          alert("complete by "+details.payer.name.given_name)
-          console.log(details)
-        })
-      },
-    }).render('#paypal-button-container');
-  },[])
-// START ///
+  useEffect(() => {
+    window.paypal
+      .Buttons({
+        creteOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: 600.0,
+                },
+              },
+            ],
+          });
+        },
+        onApprove: function (data, actions) {
+          console.log("done");
+          return actions.order.capture().then((details) => {
+            window.open("http://osamaelkassaby.com");
+            alert("complete by " + details.payer.name.given_name);
+            console.log(details);
+          });
+        },
+      })
+      .render("#paypal-button-container");
+  }, []);
+  // START ///
 
+  /*code here */
 
-/*code here */
-
-
-//HERER///
-  return (
-
-    <div id="paypal-button-container">
-          <input type='text' />
-
-    </div>
-    
-  );
+  //HERER///
+  return <div id="paypal-button-container"></div>;
 }
 
 export default App;
